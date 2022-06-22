bc4621509b84701672716357a3d862c2b85e8c3d
Shane B <shane@techelix.com>
2022-06-22 18:50:31 +0100

    Fixed incorrect result check. Fixed format of output string

--- Grading/tester.py
+++ Grading/tester.py
@@ -12,7 +12,7 @@ results_dict = {
     '5_simple_sum.txt': 6,
     '6_simple_if.txt': 15,
     '7_nested_if.txt': 16,
-    '8_if__equal_example.txt': "False",
+    '8_if__equal_example.txt': "True",
     '9_nested_add.txt': 9,
     'X_complex_sum_and_if.txt': 23
 }
@@ -24,4 +24,4 @@ print(f"{'Filename' : <25}{'Expexted' : ^25}{'Actual' : ^25}")
 for file in files:
     current_file = os.path.join(directory, file)
     output = subprocess.check_output(f"{script_command} {current_file}", encoding='UTF-8').strip()
-    print(f"{file : <25}{output : ^25}{results_dict[file] : ^25}")
+    print(f"{file : <25}{results_dict[file] : ^25}{output : ^25}")
